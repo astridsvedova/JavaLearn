1f08361b91667aaa7c777fe6be5d786ad649063c
Astrid <astridsvedova@gmail.com>
2026-05-31 23:53:16 +0200

    change comments to english, made Topic01Methods

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Topic01Methods.run();
-        // Topic02Arrays.run();
-        // Topic03OOP.run();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("=====Java Learn=====");
+        System.out.println("1 - Methods");
+        System.out.println("2 - Arrays");
+        System.out.print("Choose thema: ");
+
+        int choice = scanner.nextInt();
+
+        switch (choice) {
+            case 1 -> Topic01Methods.run();
+            case 2 -> Topic02Arrays.run();
+            default -> System.out.println("These thema doesn't exist");
+        }
+        scanner.close();
     }
 }
--- src/Topic01Methods.java
+++ src/Topic01Methods.java
@@ -1,12 +1,80 @@
 public class Topic01Methods {
 
     public static void run() {
-        System.out.println("=== Тема: Методы ===");
-        // весь мой код по теме Методы здесь
-        printSum(3, 5);
+        System.out.println("===== Theme: Methods =====");
+
+        // 1. void - void method lol no returns.
+        greet("Astrid");
+
+        // 2. return - The method returns a value.
+        System.out.println("55 + 33 = " + add(55, 33));
+        System.out.println("55 + 33 = " + multiply(55, 33));
+        System.out.println("25 / 5 = " + divide(25, 5));
+
+        // 3. several parameters of different types.
+        printPersonInfo("Astrid", 23);
+
+        // 4. a method that calls another method.
+        System.out.println("Perimeter of a Rectangle: " + perimeter(4, 5));
+
+        // 5. method using String
+        System.out.println(shout("Hello!^^"));
+
+        // 6. method with boolean
+        System.out.println("Is 5 even? " + isEven(5));
+        System.out.println("Is 8 even? " + isEven(8));
+
+        // 7. methods overloading
+        System.out.println(describe(42));
+        System.out.println(describe("Java"));
+        System.out.println(describe(3.14));
+    }
+    // void - don't return anything
+    static void greet(String name) {
+        System.out.println("Hello, " + name + "!");
     }
 
-    static void printSum(int a, int b) {
-        System.out.println(a + b);
+    // return int
+    static int add(int a, int b) {
+        return a + b;
+    }
+
+    static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    static int divide(int a, int b) {
+        return a / b;
+    }
+
+    // several parameters of different types
+    static void printPersonInfo(String name, int age) {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+
+    // the method calls another method internally.
+    static int perimeter(int width, int height) {
+        return multiply(2, add(width, height));
+    }
+    // working with Strings
+    static String shout(String text) {
+        return text.toUpperCase() +"!!!";
+    }
+    // returns a boolean
+    static boolean isEven(int n) {
+        return n % 2 == 0;
+    }
+    // Overloading — same name, different parameters,
+    static String describe(int n) {
+        return "This is a number: " + n;
+    }
+
+    static String describe(String s) {
+        return "his is a string: " + s;
+    }
+
+    static String describe(double d) {
+        return "This is a fraction: " + d;
     }
 }
